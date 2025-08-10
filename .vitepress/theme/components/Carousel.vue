6b09fa45160a62c956f60b9f535f8e092330a689
ygpynet <ygpynet@gmail.com>
2025-08-10 21:03:28 +0800

    2025年8月10日

--- .vitepress/theme/components/Carousel.vue
+++ .vitepress/theme/components/Carousel.vue
@@ -10,8 +10,8 @@
 import { FwbCarousel } from './index'
 
 const pictures = [
-    { src: '/images/vpn/paid/hongxing/hong-xing-yun.webp', alt: '红杏云', link: 'https://hongxingyun5.vip/web/#/login?code=CPXZlpWz' },
-    { src: '/images/vpn/paid/candytally/tang-guo-yun.webp', alt: '糖果云', link: 'https://candytally.diy/web/#/login?code=foHbXFgg' },
-    { src: '/images/vpn/paid/xueshan/xue-shan-ji-chang.webp', alt: '雪山机场', link: 'https://x2.xueshan.shop/#/register?code=pq7dmt6E' }
+    { src: 'https://i.imgur.com/WgsUHgz.png', alt: '红杏云', link: 'https://hongxingyun5.vip/web/#/login?code=CPXZlpWz' },
+    { src: 'https://i.imgur.com/3zTLIL3.png', alt: '糖果云', link: 'https://candytally.diy/web/#/login?code=foHbXFgg' },
+    { src: 'https://i.imgur.com/r6qD0zU.png', alt: '雪山机场', link: 'https://x2.xueshan.shop/#/register?code=pq7dmt6E' }
 ]
 </script>
--- .vitepress/theme/config/ads.js
+++ .vitepress/theme/config/ads.js
@@ -1,12 +1,12 @@
 export default {
     sidebar: {
         href: 'https://f0.yyds1-doraemon.store/#/register?code=7j3dYEtw',
-        src: '/images/vpn/paid/vpnong/20250713.webp',
+        src: 'https://i.imgur.com/4aUR7sp.png',
         alt: '哆啦 A 梦'
     },
     aside: {
         href: 'https://superbiu.com/#/register?code=KH6xv0ou',
-        src: '/images/vpn/paid/superbiu/20250414.gif',
+        src: 'https://i.imgur.com/l1XvBRi.gif',
         alt: 'Superbiu',
         text: '观影、炒币、游戏无卡顿<br>爽感到达巅峰<br>使用优惠劵 ygpy 享 9 折',
         more: '/vpn/paid#superbiu'
