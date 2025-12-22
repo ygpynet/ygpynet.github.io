28c1ed56c28767af1d16e2d25fa3981d04213d4d
clashaka <clashaka@outlook.com>
2025-12-22 23:40:30 +0800

    2025年12月22日

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,57 +1,56 @@
-import { defineConfig } from 'vitepress'
-import tailwindcss from '@tailwindcss/vite'
-import head from './theme/config/head';
-import themeConfig from './theme/themeConfig';
+import { defineConfig } from "vitepress";
+import tailwindcss from "@tailwindcss/vite";
+import head from "./theme/config/head";
+import themeConfig from "./theme/themeConfig";
 
 // 不要“/”结尾
-const webSiteUrl = 'https://ygpy.net';
+const webSiteUrl = "https://ygpy.net";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "一个朋友",
-  description: "出海工具推荐网站。涵盖机场推荐、软件列表、网址导航。一站式解决你的出海难题。",
+  description:
+    "出海工具推荐网站。涵盖机场推荐、软件列表、网址导航。一站式解决你的出海难题。",
   head,
-  lang: 'zh-CN',
-  srcDir: './src',
+  lang: "zh-CN",
+  srcDir: "./src",
   // outDir: 'C:/Users/lzl22/Desktop/dist',
   metaChunk: true,
   lastUpdated: true,
   markdown: {
     container: {
-      tipLabel: '提示',
-      warningLabel: '警告',
-      dangerLabel: '危险',
-      infoLabel: '信息',
-      detailsLabel: '详细信息'
+      tipLabel: "提示",
+      warningLabel: "警告",
+      dangerLabel: "危险",
+      infoLabel: "信息",
+      detailsLabel: "详细信息",
     },
     image: {
-      lazyLoading: true
-    }
+      lazyLoading: true,
+    },
   },
   vite: {
-    plugins: [
-      tailwindcss()
-    ],
+    plugins: [tailwindcss()],
     server: {
-      host: '0.0.0.0',
+      host: "0.0.0.0",
       port: 3000,
-      cors: true
-    }
+      cors: true,
+    },
   },
   sitemap: {
     hostname: webSiteUrl,
-    lastmodDateOnly: false
+    lastmodDateOnly: false,
   },
   transformPageData(pageData) {
     const canonicalUrl = `${webSiteUrl}/${pageData.relativePath}`
-      .replace(/index\.md$/, '')
-      .replace(/\.md$/, '.html')
+      .replace(/index\.md$/, "")
+      .replace(/\.md$/, ".html");
 
-    pageData.frontmatter.head ??= []
+    pageData.frontmatter.head ??= [];
     pageData.frontmatter.head.push([
-      'link',
-      { rel: 'canonical', href: canonicalUrl }
-    ])
+      "link",
+      { rel: "canonical", href: canonicalUrl },
+    ]);
   },
-  themeConfig
-})
+  themeConfig,
+});
---
+++ src/vpn/free/2025/12/ci-yuan-yu.md
@@ -0,0 +1,71 @@
+---
+date: 2025-12-22T23:39:07Z
+title: 试用机场推荐：次元域
+image:
+  src: https://i.imgur.com/U8U2Vrj.png
+  alt: 次元域
+subheading: 次元域
+description: 使用优惠券 0 元购买 100GB️/30 天套餐。
+hyperlink: https://cygw.ciyuan78.help/#/register?code=3uNXKoOc
+status: false
+head:
+  - - meta
+    - property: og:title
+      content: 试用机场推荐：次元域
+  - - meta
+    - property: og:description
+      content: 使用优惠券 0 元购买 100GB️/30 天套餐。
+  - - meta
+    - property: og:url
+      content: https://ygpy.net/vpn/free/2025/12/ci-yuan-yu.html
+  - - meta
+    - property: og:image
+      content: https://i.imgur.com/U8U2Vrj.png
+  - - meta
+    - property: og:image:alt
+      content: 试用机场推荐：次元域
+---
+
+# 次元域 <Badge type="info" text="试用机场" />
+
+## 介绍
+
+<ClientOnly>
+  <Img
+    src="https://i.imgur.com/U8U2Vrj.png"
+    :status="$frontmatter.status"
+  />
+</ClientOnly>
+
+使用优惠券 <ClientOnly><Tooltip code="@ciyuantupo" /></ClientOnly> 0 元购买“入门款”100GB️/30 天套餐。
+
+节点位置：香港、美国、日本、俄罗斯、加拿大
+
+协议类型：Hysteria
+
+<p>注册地址：<ClientOnly><Link :href="$frontmatter.hyperlink" /></ClientOnly></p>
+
+## 联系方式
+
+<p>频道：<ClientOnly><Link href="https://t.me/ciyuangs" /></ClientOnly></p>
+<p>群组：<ClientOnly><Link href="https://t.me/ciyuantupo" /></ClientOnly></p>
+
+## 详情信息
+
+<ClientOnly>
+  <Img
+    src="https://i.imgur.com/1yx3xXg.png"
+    alt="“次元域”价格详情"
+    :status="$frontmatter.status"
+  />
+</ClientOnly>
+<p></p>
+<ClientOnly>
+  <Img
+    src="https://i.imgur.com/lguBjVn.png"
+    alt="“次元域”测试报告"
+    :status="$frontmatter.status"
+  />
+</ClientOnly>
+
+## 评论
