42cc1f1af9791f3c8326d65c9e14e6b9c42aa189
ygpynet <ygpynet@gmail.com>
2026-03-13 20:22:54 +0800

    2026年3月13日

--- .vitepress/theme/themeConfig.js
+++ .vitepress/theme/themeConfig.js
@@ -1,50 +1,50 @@
-import nav from './config/nav';
-import sidebar from './config/sidebar';
-import footer from './config/footer';
+import nav from "./config/nav";
+import sidebar from "./config/sidebar";
+import footer from "./config/footer";
 
 export default {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: {
-        src: '/images/logo.webp',
-        alt: '一个朋友 Logo'
+  // https://vitepress.dev/reference/default-theme-config
+  logo: {
+    src: "/images/logo.webp",
+    alt: "一个朋友 Logo",
+  },
+  nav,
+  sidebar,
+  outline: {
+    label: "页面导航",
+  },
+  footer,
+  lastUpdated: {
+    text: "最后更新于",
+  },
+  search: {
+    provider: "local",
+    options: {
+      translations: {
+        button: {
+          buttonText: "搜索文档",
+          buttonAriaLabel: "搜索文档",
+        },
+        modal: {
+          noResultsText: "无法找到相关结果",
+          resetButtonTitle: "清除查询条件",
+          footer: {
+            selectText: "选择",
+            navigateText: "切换",
+            closeText: "关闭",
+          },
+        },
+      },
     },
-    nav,
-    sidebar,
-    outline: {
-        label: '页面导航'
-    },
-    footer,
-    lastUpdated: {
-        text: '最后更新于'
-    },
-    search: {
-        provider: 'local',
-        options: {
-            translations: {
-                button: {
-                    buttonText: '搜索文档',
-                    buttonAriaLabel: '搜索文档'
-                },
-                modal: {
-                    noResultsText: '无法找到相关结果',
-                    resetButtonTitle: '清除查询条件',
-                    footer: {
-                        selectText: '选择',
-                        navigateText: '切换',
-                        closeText: '关闭'
-                    }
-                }
-            }
-        }
-    },
-    docFooter: {
-        prev: '上一页',
-        next: '下一页'
-    },
-    darkModeSwitchLabel: '主题',
-    lightModeSwitchTitle: '切换到浅色模式',
-    darkModeSwitchTitle: '切换到深色模式',
-    sidebarMenuLabel: '菜单',
-    returnToTopLabel: '回到顶部',
-    externalLinkIcon: true
-}
+  },
+  docFooter: {
+    prev: "上一页",
+    next: "下一页",
+  },
+  darkModeSwitchLabel: "主题",
+  lightModeSwitchTitle: "切换到浅色模式",
+  darkModeSwitchTitle: "切换到深色模式",
+  sidebarMenuLabel: "菜单",
+  returnToTopLabel: "回到顶部",
+  externalLinkIcon: true,
+};
