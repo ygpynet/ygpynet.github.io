95d1d8f86df7b6f2e48e057d137b25505bcfb5d0
ygpynet <ygpynet@gmail.com>
2026-03-21 19:33:36 +0800

    new file:   src/disclaimer.md

--- .vitepress/config/themeConfig.js
+++ .vitepress/config/themeConfig.js
@@ -13,8 +13,8 @@ export default {
     label: "页面导航",
   },
   footer: {
-    message: `<a href="/about">关于我们</a> | <a href="/link-exchange">友情链接</a> | <a href="/privacy-policy">隐私权政策</a>`,
-    copyright: `© ${new Date().getFullYear()} 一个朋友`,
+    message: `<a href="/about">关于我们</a> | <a href="/link-exchange">友情链接</a> | <a href="/disclaimer">免责声明</a> | <a href="/privacy-policy">隐私权政策</a>`,
+    copyright: `© ${new Date().getFullYear()} 一个朋友 CC BY-NC-ND 4.0`,
   },
   lastUpdated: {
     text: "最后更新于",
