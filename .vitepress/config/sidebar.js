61f4bbd2f2b3cf4b566459b869800d4b7f537d9f
ygpynet <ygpynet@gmail.com>
2026-05-08 20:47:34 +0800

    2026年5月8日

--- .vitepress/config/sidebar.js
+++ .vitepress/config/sidebar.js
@@ -11,6 +11,10 @@ export default {
               collapsed: false,
               items: [
                 {
+                  text: "5 月",
+                  link: "/vpn/test/2026/05",
+                },
+                {
                   text: "4 月",
                   link: "/vpn/test/2026/04",
                 },
