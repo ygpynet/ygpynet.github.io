6f720065fca3b8b8bfce8938776079728e16e0cd
ygpynet <ygpynet@gmail.com>
2026-06-16 22:40:10 +0800

    2026年6月16日

--- .vitepress/config/sidebar.js
+++ .vitepress/config/sidebar.js
@@ -38,11 +38,91 @@ export default {
             },
             {
               text: "2025 年",
-              link: "/vpn/test/2025",
+              collapsed: true,
+              items: [
+                {
+                  text: "12 月",
+                  link: "/vpn/test/2025/12",
+                },
+                {
+                  text: "11 月",
+                  link: "/vpn/test/2025/11",
+                },
+                {
+                  text: "10 月",
+                  link: "/vpn/test/2025/10",
+                },
+                {
+                  text: "9 月",
+                  link: "/vpn/test/2025/09",
+                },
+                {
+                  text: "8 月",
+                  link: "/vpn/test/2025/08",
+                },
+                {
+                  text: "7 月",
+                  link: "/vpn/test/2025/07",
+                },
+                {
+                  text: "6 月",
+                  link: "/vpn/test/2025/06",
+                },
+                {
+                  text: "5 月",
+                  link: "/vpn/test/2025/05",
+                },
+                {
+                  text: "4 月",
+                  link: "/vpn/test/2025/04",
+                },
+                {
+                  text: "3 月",
+                  link: "/vpn/test/2025/03",
+                },
+                {
+                  text: "2 月",
+                  link: "/vpn/test/2025/02",
+                },
+                {
+                  text: "1 月",
+                  link: "/vpn/test/2025/01",
+                },
+              ],
             },
             {
               text: "2024 年",
-              link: "/vpn/test/2024",
+              collapsed: true,
+              items: [
+                {
+                  text: "12 月",
+                  link: "/vpn/test/2024/12",
+                },
+                {
+                  text: "11 月",
+                  link: "/vpn/test/2024/11",
+                },
+                {
+                  text: "10 月",
+                  link: "/vpn/test/2024/10",
+                },
+                {
+                  text: "9 月",
+                  link: "/vpn/test/2024/09",
+                },
+                {
+                  text: "8 月",
+                  link: "/vpn/test/2024/08",
+                },
+                {
+                  text: "7 月",
+                  link: "/vpn/test/2024/07",
+                },
+                {
+                  text: "6 月",
+                  link: "/vpn/test/2024/06",
+                },
+              ],
             },
           ],
         },
